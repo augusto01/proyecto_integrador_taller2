3fa8dc68e978b0d1c2ef83085d6d6e0fff743609
Almiron Augusto <79406262+augusto01@users.noreply.github.com>
2023-11-01 06:33:10 -0300

    intento3_procedimientos_almacenados

---
+++ CapaDatos/CD_Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad.Cache;
+
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class CD_Usuario
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["sql"].ConnectionString);
+
+        public DataTable D_listar_usuarios()
+        {
+            SqlCommand cmd = new SqlCommand("sp_listar_usuarios", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+
+        }
+
+        public DataTable D_buscar_usuario(CapaEntidad.Cache.UserLoginCache obje)
+        {
+            SqlCommand cmd = new SqlCommand("sp_buscar_usuarios", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@dni_usuario",obje.d);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+
+        }
+    }
+}
+}
---
+++ CapaEntidad/CE_Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace CapaEntidad
+{
+    public class loguin_entidad
+    {
+
+       public string username {get;set;}
+
+       public string pass { get; set; }
+
+
+    }
+
+
+    public class UsuarioEntidad
+    {
+
+        public static int id_usuario { get; set; }
+        public static int id_tipo_usuario { get; set; }
+        public static string domicilio { get; set; }
+        public static string nombre { get; set; }
+        public static string apellido { get; set; }
+        public static string correo { get; set; }
+
+        public static DateTime fecha_creacion { get; set; }
+        public static int dni { get; set; }
+        public static string username { get; set; }
+        public static string pass { get; set; }
+        public static int cel { get; set; }
+
+        public static SqlBoolean estado { get; set; }
+
+
+
+    }
+}
